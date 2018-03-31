387e87fe201efc2e7cdd8191886a55f2e47efb8c
Kim Il Sik <kimilsik@Kims-MacBook.local>
2018-03-31 19:57:29 +0900

    create rekog collection update

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,7 +5,7 @@ mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_AMBER_URI);
 mongoose.connection.on('error', (err) => {
     console.error(err);
     console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
-process.exit();
+    process.exit();
 });
 
 console.log('mongoose setting complete.');
