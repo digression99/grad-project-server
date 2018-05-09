20c8429c64e0b9cbc9a869d237f4d5b6755a51c6
Kim Il Sik <kimilsik@Kims-MacBook.local>
2018-05-09 13:32:03 +0900

    register user no user error fix

--- api/user.ctrl.js
+++ api/user.ctrl.js
@@ -28,8 +28,8 @@ exports.registerUser = async (req, res) => {
     console.log("token : ", token);
 
     try {
-        const user = User.findByEmail(email);
-        if (user) throw new Error('user already exists.');
+        // const user = User.findByEmail(email);
+        // if (user) throw new Error('user already exists.');
         if (!email || !password || !token) throw new Error('no data accepted.');
 
         await saveUserToDB(email, password, token);
